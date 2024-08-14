4da5319d135a80dbc539340f5b19927a8ed751eb
pritamUem <pb7804162@gmail.com>
2024-08-15 03:05:33 +0530

    yes

--- variable.go
+++ variable.go
@@ -1,20 +1,13 @@
-// Go program to illustrate
-// concept of variable
 package main
 
 import "fmt"
 
 func main() {
 
-	// Variable declared and
-	// initialized without the
-	// explicit type
 	var myvariable1 = 20
 	var myvariable2 = "GeeksforGeeks"
 	var myvariable3 = 34.80
 
-	// Display the value and the
-	// type of the variables
 	fmt.Printf("The value of myvariable1 is : %d\n",
 		myvariable1)
 
